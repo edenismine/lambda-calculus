d35ff7f113ad5463874a0363bee55f7a236f4b7d
Daniel Aragon <daniel.aragon@ciencias.unam.mx>
2018-10-04 18:01:48 -0500

    impemented frVars

--- src/UntypedLambda.hs
+++ src/UntypedLambda.hs
@@ -11,7 +11,7 @@
 
 module UntypedLambda (
   Identifier,
-  Expr,
+  Exp,
   frVars,
   lkVars,
   incrVar,
@@ -26,26 +26,26 @@ import Data.List
 
 type Identifier = String
 
-data Expr = Var Identifier | Lam Identifier Expr | App Expr Expr deriving Eq
+data Exp = Var Identifier | Lam Identifier Exp | App Exp Exp deriving Eq
 
 --Creando una instancia de la clase show.
-instance Show Expr where
+instance Show Exp where
   show e = case e of
     Var x   -> x
     Lam x e -> "\\"++ x ++ " -> " ++ show e
     App x y -> "("++show x ++ " " ++ show y ++ ")"
 
 --Definiendo el tipo de sustitución.
-type Substitution = (Identifier , Expr)
+type Substitution = (Identifier , Exp)
 
 -- | frVars
 -- | Función que obtiene el conjunto de variables libres de una expresión.
-frVars :: Expr -> [Identifier]
-frVars e = error "implementar"
+frVars :: Exp -> [Identifier]
+frVars = dedup . fvAux [] []
 
 -- | lkVars
 -- | Función que obtiene el conjunto de variables de una expresión.
-lkVars :: Expr -> [Identifier]
+lkVars :: Exp -> [Identifier]
 lkVars e = error "Implementar"
 
 -- | incrVar
@@ -59,12 +59,12 @@ incrVar e = error "implementar"
 -- | Función que toma una expresión lambda
 -- | y devuelve una α-equivalencia utilizando la función incrVar
 -- | hasta encontrar un nombre que no aparezca en el cuerpo.
-alphaExpr :: Expr -> Expr
+alphaExpr :: Exp -> Exp
 alphaExpr e = error "Implementar"
 
 -- | subst
 -- | Función que aplica la sustitución a la expresión dada.
-subst :: Expr -> Substitution -> Expr
+subst :: Exp -> Substitution -> Exp
 subst e s = error "Implementar"
 
 --------------------------------------------------
@@ -73,21 +73,31 @@ subst e s = error "Implementar"
 
 -- | beta.
 -- | Función que aplica un paso de la beta reducción.
-beta :: Expr -> Expr
+beta :: Exp -> Exp
 beta e = error "Implementar"
 
 -- | locked.
 -- | Función que determina si una expresión está bloqueada,
 -- | es decir, no se pueden hacer más beta reducciones.
-locked :: Expr -> Bool
+locked :: Exp -> Bool
 locked e = error "Implementar"
 
 -- | eval.
 -- | Función que evalúa una expresión lambda
 -- | aplicando beta reducciones hasta quedar bloqueada.
-eval :: Expr -> Expr
+eval :: Exp -> Exp
 eval e = error "Implementar"
 
 {-  FUNCIONES AUXILIARES  -}
+
+-- |dedup.
+-- Esta función remueve duplicados de una lista en tiempo O(n log n)
 dedup :: Ord a => [a] -> [a]
 dedup = map head . group . sort
+
+-- |fvAux.
+fvAux :: [Identifier] -> [Identifier] -> Exp -> [Identifier]
+fvAux free bounded exp = case exp of
+  Var x   -> if x `elem` bounded then free else x : free
+  Lam x e -> fvAux free (x:bounded) e
+  App x y -> fvAux free bounded x `union` fvAux free bounded y
